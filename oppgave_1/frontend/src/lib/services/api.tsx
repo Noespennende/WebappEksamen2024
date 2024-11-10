134a9ce811332fbd4e9b9a8357c945342c838ff0
M1kke95 <m1kke95@hotmail.com>
2024-11-10 19:30:37 +0100

    getLesson fetch
    Type er for øyeblikket any, kan vurdere å endre det til en type vi setter senere, fetch linken er også midlertidig

---
+++ oppgave_1/frontend/src/lib/services/api.tsx
@@ -0,0 +1,16 @@
+
+
+export const getLesson = async (courseSlug: any, lessonSlug:any) => {
+    try {
+        const response = await fetch(`https://localhost:4000/lessons?course=${courseSlug}&lesson=${lessonSlug}`)
+
+        if(!response.ok){
+            throw new Error("Feil ved fetch av Lesson" + response.statusText)
+        }
+        const responseJson = await response.json()
+        
+        return responseJson
+    } catch (error) {
+        throw error
+    }
+}
