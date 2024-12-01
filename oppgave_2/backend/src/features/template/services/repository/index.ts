aad3c5fb1bd8e28ea0b93721e7b935db3ade7d8b
M1kke95 <m1kke95@hotmail.com>
2024-12-01 21:03:16 +0100

    Feat: Template repository
    La til funksjonene som trengs i repository
    
    getAll
    getOne
    create
    
    i prismaschema lagde jeg en ny model som inneholder weekdays i string verdi som blir satt som array i templatebaseschema

--- oppgave_2/backend/src/features/template/services/repository/index.ts
+++ oppgave_2/backend/src/features/template/services/repository/index.ts
@@ -10,15 +10,93 @@
 
 */
 
-import { CreateParticipant } from '@/types';
+import { CreateParticipant, Result, Weekday } from '@/types';
 import { PrismaClient } from '@prisma/client'
+import { CreateTemplate, Template } from '../..';
+import { UUID } from 'crypto';
 
 const prisma = new PrismaClient()
 
-export const createParticipant = async (data: CreateParticipant) => {
-  return prisma.participant.create({ data });
-};
+export const createTemplateRepository = () => {
+  return{
 
-export const findParticipantById = async (id: string) => {
-  return prisma.participant.findUnique({ where: { id } });
-};
+    async getAllTemplates() {
+      try {
+        const prismadata = await prisma.templateBaseSchema.findMany({
+          where:{
+            isPrivate: false
+          },
+          include: {
+            fixedWeekDays:{
+              select: {
+                weekdays:true
+              }
+            }
+          }
+          
+        })
+
+        const result: Result<Template> = {success:true, data: prismadata}
+      } catch (error) {
+        const result: Result<null> = { success: false, error: { code: "INTERNAL_SERVER_ERROR", message: "Failed to fetch Templates" } };
+        return result;
+      }
+    },
+
+    async getOneTemplate(id: UUID){
+      try {
+        const prismadata = await prisma.templateBaseSchema.findUnique({
+          where: {id: id}
+        })
+
+        const result: Result<Template> = {success: true, data: prismadata}
+        return result
+      } catch (error) {
+        const result: Result<null> = {success: false, error: {code: "INTERNAL_SERVER_ERROR", message: "Failed to fetch Templates"}}
+        return result
+      }
+    },
+
+    async createTemplate(data: {
+      id: string,
+      name: string,
+      isPrivate: boolean,
+      fixedPrice: boolean,
+      allowSameDayEvent: boolean,
+      waitList: boolean,
+      limitedParticipants: boolean,
+      fixedWeekDays: Weekday[],
+      price: number,
+      maxParticipants: number
+     }){
+      try {
+        const prismadata = await prisma.templateBaseSchema.create({
+          data:{
+            id: data.id,
+            name: data.name,
+            isPrivate: data.isPrivate,
+            fixedPrice: data.fixedPrice,
+            allowSameDayEvent: data.allowSameDayEvent,
+            waitList: data.waitList,
+            limitedParticipants: data.limitedParticipants,
+            fixedWeekDays: {
+              create: data.fixedWeekDays.map(day => ({
+                weekdays: day
+              }))
+            },
+            price: data.price,
+            maxParticipants: data.maxParticipants
+            
+          }
+        })
+        const result: Result<Template> = {success: true, data: prismadata}
+        return result
+      } catch (error) {
+        const result: Result<null> = {success: false, error: {code: "INTERNAL_SERVER_ERROR", message: "Failed to create template"}}
+        return result
+      }
+    }
+
+
+  }
+}
