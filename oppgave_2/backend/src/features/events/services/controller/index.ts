d45e08b8940b6ca2fd93c340d1c5dca3d885a4da
Andreasberg89 <andber9@gmail.com>
2024-12-03 13:29:04 +0100

    bugfix: removed wrong console log

--- oppgave_2/backend/src/features/events/services/controller/index.ts
+++ oppgave_2/backend/src/features/events/services/controller/index.ts
@@ -48,7 +48,6 @@ export const createEventController = (occasionService: OccasionService) => {
         try {
             const slug = context.req.param(getOneParam)
             const data = await occasionService.getOccasionById(slug) 
-            console.log(data[0].body)
             return context.json(data)
             if(!data.success){
                 return errorResponse(context, data.error.code, data.error.message)
